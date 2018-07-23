f4713b86156416a771bfe098f24e3235da9dedde
Unknown <shan.lingeswaran@hotmail.co.uk>
2018-07-23 16:56:50 +0100

    did a check for a new func which calcs points on the circ around the void locations

--- Sirkit/Sirkit/ViewController.swift
+++ Sirkit/Sirkit/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Foundation
 
 
 
@@ -17,7 +18,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
 var locationManager = CLLocationManager()
 var currentLocation: CLLocation?
-let userDefaults = UserDefaults.standard
+var points1: [[CLLocationCoordinate2D]] = []
+    
 
     var array : [String]?
     var holder : [Any] = []
@@ -157,6 +159,19 @@ let userDefaults = UserDefaults.standard
        // let location = CLLocation(latitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!)
         //let circleCenter  = CLLocation(latitude: CLLocationDegrees(reducedLat[0]), longitude: CLLocationDegrees(reducedLong[0]))
       
+        //for i in 0..<lat.count{
+
+        points1.append(radiusSearchPoints(coordinate: CLLocationCoordinate2DMake(CLLocationDegrees(reducedLat[0]), CLLocationDegrees(reducedLong[0])), radius: 10))
+        // }//makes an array of coords which lay on the circumference of the original circle
+        print(points1)
+        
+        
+        
+        
+       
+        
+       
+        
         
         for i in 0..<lat.count {
             let locations1 = customPin(pinTitle: reducedType[i]+" "+reducedSpeed[i], pinSubTitle: "", location: CLLocationCoordinate2DMake(CLLocationDegrees(reducedLat[i]), CLLocationDegrees(reducedLong[i])))
@@ -166,12 +181,12 @@ let userDefaults = UserDefaults.standard
             self.mapView.addOverlays(circles)
         }
         mapView.delegate = self//absolutely necessry!
+        
+        
+        
     
        
-
-        
-        
-        
+       
         
   
         
@@ -180,7 +195,42 @@ let userDefaults = UserDefaults.standard
     }//end of viewDidLoad
         
     
- 
+    internal func radiusSearchPoints(coordinate: CLLocationCoordinate2D, radius: CLLocationDistance) -> [CLLocationCoordinate2D]
+    {
+        var points: [CLLocationCoordinate2D] = []
+        let earthRadius = 6378100.0
+        let π = M_PI
+        
+        let lat = coordinate.latitude * π / 180.0
+        let lng = coordinate.longitude * π / 180.0
+        
+        for t in stride(from: 0, to: 2 * π, by: 0.3){
+            let pointLat = lat + (radius / earthRadius)  * sin(t)
+            let pointLng = lng + (radius / earthRadius) * cos(t)
+            
+            var circPointLat : [Double] = []
+            var circPointLong : [Double] = []
+            
+            circPointLat.append((pointLat * 180 / π))
+            circPointLong.append((pointLng * 180 / π))
+            
+            let point = CLLocationCoordinate2D(latitude: (pointLat * 180 / π), longitude: (pointLng * 180 / π))
+            points.append(point)
+            
+          //  for i in 0..<points.count {
+            //    let locations2 = customPin(pinTitle: "CircPoint", pinSubTitle: "", location: points[i])
+              //   self.mapView.addAnnotation(locations2)
+           // } This bit of code plots pins on the circle that th system will try avoid
+            
+            
+            
+        }
+        return points
+    }//this takes a centre poit and radius to provide a number of points on the circumference of the circle
+    
+    
+
+    
     
     
     
@@ -196,13 +246,15 @@ let userDefaults = UserDefaults.standard
         print("locations = \(locations.latitude) \(locations.longitude)")
         
         
+      //  while locationManager.location?.coordinate != nil {
+            //while
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region = MKCoordinateRegion(center: CLLocationCoordinate2DMake((locationManager.location?.coordinate.latitude)!, (locationManager.location?.coordinate.longitude)!), span: span)
         self.mapView.setRegion(region, animated: true)
         
         //attempt at routing by avoiding pin coords
         let sourceLoc = (locationManager.location?.coordinate)!
-        let destLoc = CLLocationCoordinate2DMake(51.5033640, -0.1276250)
+        let destLoc = CLLocationCoordinate2DMake(51.559104919433601, 0.13010259757630524)//    51.5033640  -0.1276250
         
         let sourcePlacemark = MKPlacemark(coordinate: sourceLoc)
         let destPlacemark = MKPlacemark(coordinate: destLoc)
@@ -232,8 +284,8 @@ let userDefaults = UserDefaults.standard
             self.mapView.setRegion(MKCoordinateRegionForMapRect(rekt), animated: true)
         })
         
-        
-    }
+        }
+   // }
     
     func locationManagerError(_manager: CLLocationManager, didFailWithError error: Error) {
         print("Unable to access your current location")
